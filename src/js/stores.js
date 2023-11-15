2516fb7efb9fa86f08ca95335dac362791b2bec9
Zuev <dszuev@bellintegrator.com>
2023-11-15 22:51:23 +0300

    fix

--- src/js/stores.js
+++ src/js/stores.js
@@ -79,7 +79,7 @@ document.addEventListener("DOMContentLoaded", () => {
     "catalog-restaurants-list-loader"
   );
 
-  fetch(window.location.origin + "api/restaurants")
+  fetch(window.location.origin + "/api/restaurants")
     .then((res) => {
       return res.json();
     })
